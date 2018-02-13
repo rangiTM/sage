bd9820677aacbdca93864828cc950c4503071503
Rangi TM <gully1218@me.com>
2018-02-13 13:37:45 +0900

    initial commit

--- lib/setup.php
+++ lib/setup.php
@@ -100,7 +100,8 @@ function assets() {
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
-
+ 
+  wp_enqueue_script('sage/popper', Assets\asset_path('scripts/popper.js'), [], null, true);
   wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
